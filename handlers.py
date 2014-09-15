4a0015184fd11d908d4ebafb01ee5475d452a999
blackshirt <blackshirt@yahoo.com>
2014-09-15 15:18:18 +0700

    Signed-off-by: blackshirt <blackshirt@yahoo.com>

--- handlers.py
+++ handlers.py
@@ -1,108 +1,133 @@
-import tornado.web
+import datetime
+import tornado.web, tornado.escape
 import tornado.wsgi
-
+import os
 import model
+from peewee import fn
+import json
+
 
 class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/", HomeHandler),
-            (r"/archive", ArchiveHandler),
-            (r"/feed", FeedHandler),
-            (r"/entry/([^/]+)", EntryHandler),
-            (r"/compose", ComposeHandler),
-            (r"/auth/login", AuthLoginHandler),
-            (r"/auth/logout", AuthLogoutHandler),
-        ]
-        settings = dict(
-            blog_title=u"Tornado Blog",
-            template_path=os.path.join(os.path.dirname(__file__), "templates"),
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
-            ui_modules={"Entry": EntryModule},
-            xsrf_cookies=True,
-            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-            login_url="/auth/login",
-            debug=True,
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
+    def __init__(self):
+        handlers = [
+            (r"/", IndexHandler),
+            (r"/news", NewsHandler),
+            (r"/entry/([^/]+)", EntryHandler),
+            (r"/compose", ComposeHandler),
+            (r"/auth/login", AuthLoginHandler),
+            (r"/auth/logout", AuthLogoutHandler),
+        ]
 
-        # Have one global connection to the blog DB across all handlers
-        self.db = model.database
+        settings = dict(
+            blog_title=u"Tornado Blog",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            # ui_modules={"Entry": EntryModule},
+            xsrf_cookies=True,
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            login_url="/auth/login",
+            debug=True,
+        )
+
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+        # Have one global connection to the blog DB across all handlers
+        self.db = model.database
+
 
 class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def db(self):
-        return self.application.db
+    @property
+    def db(self):
+        return self.application.db
 
-    def get_current_user(self):
-        user_id = self.get_secure_cookie("blogdemo_user")
-        if not user_id: return None
-        return self.db.get("SELECT * FROM authors WHERE id = %s", int(user_id))
-        
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("blogdemo_user")
+
+        if not user_id:
+            return None
+        return self.db.get("SELECT * FROM authors WHERE id = %s", int(user_id))
+
+
 class IndexHandler(BaseHandler):
- 
     def get(self):
-        #get random news from database
-        randomnews = model.Message.select().order_by(fn.Random()).limit(1)
+        # get random news from database
+        news = model.Message.select().order_by(fn.Random()).limit(1).get()
+        news = tornado.escape.json_encode(news)
         self.set_header('Content-Type', 'application/json')
-        self.write(dumps(randomnews))
+        self.write(json.dumps(str(news)))
 
     def post(self):
         _id = () + 1
         timestamp = datetime.now()
         body = urlparse.parse_qs(self.request.body)
         for key in body:
-                body[key] = body[key][0]
+            body[key] = body[key][0]
         blog = {
-                "_id": _id,
-                "title": body['title'],
-                "tags": body['tags'],
-                "category": body['category'],
-                "timestamp": timestamp
+            "_id": _id,
+            "title": body['title'],
+            "tags": body['tags'],
+            "category": body['category'],
+            "timestamp": timestamp
         }
         self.db.blogs.insert(blog)
-        location = "/blog/"+ str(_id)
+        location = "/blog/" + str(_id)
         self.set_header('Content-Type', 'application/json')
         self.set_header('Location', location)
         self.set_status(201)
         self.write(dumps(blog))
- 
+
     def put(self, blogid):
-        ## Convert unicode to int
+        # # Convert unicode to int
         _id = int(blogid)
         timestamp = datetime.now()
         body = urlparse.parse_qs(self.request.body)
         for key in body:
-                body[key] = body[key][0]
+            body[key] = body[key][0]
         blog = {
-                "title": body['title'],
-                "tags": body['tags'],
-                "category": body['category'],
-                "timestamp": timestamp
+            "title": body['title'],
+            "tags": body['tags'],
+            "category": body['category'],
+            "timestamp": timestamp
         }
-        self.db.blogs.update({"_id":_id}, {"$set":blog})
+        self.db.blogs.update({"_id": _id}, {"$set": blog})
         self.set_header('Content-Type', 'application/json')
         self.write(dumps(blog))
- 
-    def delete(self,blogid):
-        ## Convert unicode to int
+
+    def delete(self, blogid):
+        # # Convert unicode to int
         _id = int(blogid)
         blog = {
-                "title": None,
-                "tags": [],
-                "category": [],
-                "timestamp": None,
+            "title": None,
+            "tags": [],
+            "category": [],
+            "timestamp": None,
         }
-        self.db.blogs.update({"_id":_id}, {"$set":blog})
+        self.db.blogs.update({"_id": _id}, {"$set": blog})
         self.set_header('Content-Type', 'application/json')
         self.write(dumps(blog))
 
 
+class NewsHandler(BaseHandler):
+    pass
 
-application = tornado.web.Application([
-    (r"/", IndexHandler),
-    (r"/transaksi/([0-9]+)", Blog, dict(connection = Connection()) ),
-    (r"/transaksi/", Blog, dict(connection =  Connection()) ),
-    (r"/blogs/", Blogs, dict(connection =  Connection()) ),
-], debug=True)
 
+class EntryHandler(BaseHandler):
+    pass
+
+
+class ComposeHandler(BaseHandler):
+    pass
+
+
+class AuthLoginHandler(BaseHandler):
+    pass
+
+
+class AuthLogoutHandler(BaseHandler):
+    pass
+
+
+if __name__ == "__main__":
+    application = Application()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
--- model.py
+++ model.py
@@ -2,12 +2,14 @@ import peewee
 import hashlib
 import datetime
 
-__all__ = [database, User, Message, Category, Transaksi, TeansaksiDetail]
+__all__ = ['database', 'User', 'Message', 'Category', 'Transaksi', 'TransaksiDetail']
 
-db = '/storage/sdcard1/database/dompetku.sqlite'
-#db = config['dbpath']
+#db = '/storage/sdcard1/database/dompetku.sqlite'
+db = r"D:\My Documents\db\dompetku.sqlite"
+# db = config['dbpath']
 
 database = peewee.SqliteDatabase(db)
+
 
 def gen_hash(password):
     return hashlib.sha512(str(password).encode('utf-8')).hexdigest()
@@ -17,6 +19,7 @@ class Base(peewee.Model):
     class Meta:
         database = database
 
+
 class User(Base):
     uid = peewee.PrimaryKeyField()
     name = peewee.CharField(unique=True)
@@ -25,26 +28,30 @@ class User(Base):
     currentbalance = peewee.DecimalField(default=0)
     created = peewee.DateTimeField(default=datetime.datetime.now)
     lastactive = peewee.DateTimeField(default=datetime.datetime.now)
-    
+
     class Meta:
         order_by = ('name',)
+
 
 class Message(Base):
     mid = peewee.PrimaryKeyField()
     title = peewee.CharField()
     body = peewee.TextField()
-    author = peewee.ForeignKeyField (User)
+    author = peewee.ForeignKeyField(User)
     created = peewee.DateTimeField()
-    
+
+
 class Category(Base):
     cid = peewee.PrimaryKeyField()
-    category = peeweeCharField(unique=True)
+    category = peewee.CharField(unique=True)
     desc = peewee.CharField(default='Deskripsi')
+
 
 class TipeTransaksi(Base):
     ttid = peewee.PrimaryKeyField()
     type = peewee.CharField()
     desc = peewee.CharField()
+
 
 class Transaksi(Base):
     tid = peewee.PrimaryKeyField()
@@ -55,6 +62,7 @@ class Transaksi(Base):
     transdate = peewee.DateTimeField(default=datetime.datetime.now)
     memo = peewee.CharField()
 
+
 class TransaksiDetail(Base):
     tdid = peewee.PrimaryKeyField()
     transid = peewee.ForeignKeyField(Transaksi)
@@ -64,45 +72,51 @@ class TransaksiDetail(Base):
     times = peewee.DateTimeField(default=datetime.datetime.now)
     notes = peewee.CharField()
 
+
 userdata = [
-    {'name':'paijo','password':gen_hash('paijo'),'email':'paijo@none'},
-    {'name':'black','password':gen_hash('black'),'email':'black@none'},
+    {'name': 'paijo', 'password': gen_hash('paijo'), 'email': 'paijo@none'},
+    {'name': 'black', 'password': gen_hash('black'), 'email': 'black@none'},
 ]
+
 
 def insert_user(database):
     with database.transaction():
         User.insert_many(userdata).execute()
 
-tipe_trans_data= [
-    {'type':'MSK', 'desc':'Transaksi Masuk'},
-    {'type':'OUT', 'desc':'Transaksi Keluar '},
-    {'type':'TRN', 'desc':'Transaksi Transfer'},
-    {'type':'BLN', 'desc':'Transaksi Balance'},
-    {'type':'OTH', 'desc':'Transaksi Lain'},
+
+tipe_trans_data = [
+    {'type': 'MSK', 'desc': 'Transaksi Masuk'},
+    {'type': 'OUT', 'desc': 'Transaksi Keluar '},
+    {'type': 'TRN', 'desc': 'Transaksi Transfer'},
+    {'type': 'BLN', 'desc': 'Transaksi Balance'},
+    {'type': 'OTH', 'desc': 'Transaksi Lain'},
 ]
+
 
 def insert_tipe_trans(database):
     with database.transaction():
         TipeTransaksi.insert_many(tipe_trans_data).execute()
-   
+
+
 category_data = [
-    {'category':'sandang','desc':'kebutuhan sandang'},
-    {'category':'pangan','desc':'kebutuhan pangan'},
-    {'category':'papan','desc':'kebutuhan papan'},
-    {'category':'rumahtangga','desc':'kebutuhan rumah tangga'},
-    {'category':'transport','desc':'kebutuhan transportasi'},
-    {'category':'jasa','desc':'kebutuhan jasa'},
-    {'category':'kerja','desc':'kebutuhan kerja'},
-    {'category':'masyarakat','desc':'kebutuhan masyarakat'},
-    {'category':'umum','desc':'kebutuhan umum'},
-    {'category':'lain','desc':'kebutuhan lain'},
-] 
+    {'category': 'sandang', 'desc': 'kebutuhan sandang'},
+    {'category': 'pangan', 'desc': 'kebutuhan pangan'},
+    {'category': 'papan', 'desc': 'kebutuhan papan'},
+    {'category': 'rumahtangga', 'desc': 'kebutuhan rumah tangga'},
+    {'category': 'transport', 'desc': 'kebutuhan transportasi'},
+    {'category': 'jasa', 'desc': 'kebutuhan jasa'},
+    {'category': 'kerja', 'desc': 'kebutuhan kerja'},
+    {'category': 'masyarakat', 'desc': 'kebutuhan masyarakat'},
+    {'category': 'umum', 'desc': 'kebutuhan umum'},
+    {'category': 'lain', 'desc': 'kebutuhan lain'},
+]
+
 
 def insert_category_data(database):
     with database.transaction():
         Category.insert_many(category_data).execute()
 
+
 def init():
     database.connect()
-    peewee.create_model_tables([User, Message, Category, TipeTransaksi, Transaksi, TransaksiDetail],fail_silently=True)
-    
+    peewee.create_model_tables([User, Message, Category, TipeTransaksi, Transaksi, TransaksiDetail], fail_silently=True)
