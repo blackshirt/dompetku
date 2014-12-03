aac5338deac0e88292c6286a8cafc9de8b921120
blackshirt <blackshirt@yahoo.com>
2014-12-03 14:56:21 +0700

    Some minor change on model

--- dompetku/model.py
+++ dompetku/model.py
@@ -3,8 +3,9 @@ import hashlib
 import datetime
 import os
 
-#from wtfpeewee.orm import model_form
+from wtfpeewee.orm import model_form
 from wtforms_tornado import Form
+from wtforms.validators import Length
 
 __all__ = ['database', 'User', 'Message', 'Category', 'Transaksi', 'TransaksiDetail']
 
@@ -19,18 +20,18 @@ def gen_hash(password):
     return hashlib.sha512(str(password).encode('utf-8')).hexdigest()
 
 
-class Base(peewee.Model):
+class BaseModel(peewee.Model):
     class Meta:
         database = database
 
 
-class User(Base):
+class User(BaseModel):
     uid = peewee.PrimaryKeyField()
     name = peewee.CharField(max_length=255, unique=True)
     realname = peewee.CharField(max_length=255, default='guest')
     password = peewee.CharField()
     email = peewee.CharField(max_length=255)
-    created = peewee.DateTimeField(default=datetime.datetime.now)
+    created = peewee.DateField(default=datetime.date.today)
     active = peewee.BooleanField(default=False)
     lastactive = peewee.DateTimeField(default=datetime.datetime.now)
 
@@ -38,19 +39,18 @@ class User(Base):
         order_by = ('name',)
 
 #UserForm = model_form(User, base_class=Form)
-
-class Account(Base):
+class Account(BaseModel):
     acid = peewee.PrimaryKeyField()
     name = peewee.CharField()
     amount = peewee.DecimalField()
     info = peewee.TextField()
 
 
-class Hutang(Base):
+class Hutang(BaseModel):
     hid = peewee.PrimaryKeyField()
-    tanggal = peewee.DateTimeField()
-    tempo = peewee.DateTimeField()
-    amount = peewee.DecimalField()
+    tanggal = peewee.DateField(default=datetime.date.today)
+    tempo = peewee.DateField(default=datetime.date.today)
+    amount = peewee.DecimalField(default=0)
     sisa = peewee.DecimalField(default=0)
     hutanger = peewee.CharField()
     deskripsi = peewee.TextField()
@@ -59,7 +59,7 @@ class Hutang(Base):
         order_by = ('-amount', 'tempo')
 
 
-class Piutang(Base):
+class Piutang(BaseModel):
     pid = peewee.PrimaryKeyField()
     tanggal = peewee.DateTimeField()
     tempo = peewee.DateTimeField()
@@ -72,21 +72,21 @@ class Piutang(Base):
         order_by = ('-amount', 'tempo')
 
 
-class Investasi(Base):
+class Investasi(BaseModel):
     invid = peewee.PrimaryKeyField()
     jenis = peewee.CharField()
     amount = peewee.DecimalField()
     tanggal = peewee.DateField()
 
 
-class Aset(Base):
+class Aset(BaseModel):
     asid = peewee.PrimaryKeyField()
     name = peewee.CharField()
     jenis = peewee.CharField()
     harga_sekarang = peewee.DecimalField()
 
 
-class Message(Base):
+class Message(BaseModel):
     mid = peewee.PrimaryKeyField()
     title = peewee.CharField()
     body = peewee.TextField()
@@ -98,36 +98,34 @@ class Message(Base):
 
 #MessageForm = model_form(Message, base_class=Form)
 
-class Category(Base):
+class Category(BaseModel):
     cid = peewee.PrimaryKeyField()
     category = peewee.CharField(unique=True)
     desc = peewee.CharField(default='Deskripsi')
 
 
-class TipeTransaksi(Base):
+class TipeTransaksi(BaseModel):
     ttid = peewee.PrimaryKeyField()
     tipe = peewee.CharField()
     desc = peewee.TextField()
 
 #TipeTransaksiForm = model_form(TipeTransaksi, base_class=Form)
 
-class Transaksi(Base):
+class Transaksi(BaseModel):
     tid = peewee.PrimaryKeyField()
     user = peewee.ForeignKeyField(rel_model=User)
     tipe = peewee.ForeignKeyField(rel_model=TipeTransaksi)
     info = peewee.CharField()
     amount = peewee.DecimalField(default=0)
-    transdate = peewee.DateTimeField(default=datetime.datetime.now())
-    amount = peewee.DecimalField()
     transdate = peewee.DateTimeField(default=datetime.datetime.now)
     memo = peewee.TextField()
 
     class Meta:
         order_by = ('-transdate',)
 
-#TransaksiForm = model_form(Transaksi, allow_pk=True, base_class=Form)
+TransaksiForm = model_form(Transaksi, base_class=Form, exclude=('tid','user','tipe','transdate',))
 
-class TransaksiDetail(Base):
+class TransaksiDetail(BaseModel):
     tdid = peewee.PrimaryKeyField()
     transid = peewee.ForeignKeyField(rel_model=Transaksi)
     item_transaksi = peewee.CharField()
